9d33d2e715a8f160960d17056ac60cc6ff333c5d
PavanKumars98 <96567829+PavanKumars98@users.noreply.github.com>
2021-12-23 13:37:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+    
+    function W(){
+        document.getElementById("demo").innerHTML= "It is a very simple mini project where there is a digital notice board that displays the messages that we send through our mobile phones with the help of Arduino uno  and GSM module. It can be used in various places like airports, restaurants, hospitals, etc."
+    }
+
+    function A() {
+        document.getElementById("demo1").innerHTML="This project is an alert system which sends an emergency message to an emergency contact number, when a person has met with an motor bike accident, the message is sent with the help of Arduino uno, GSM module, GPS module and vibration sensor modules." 
+    }
+
+    function S(){
+        document.getElementById("demo2").innerHTML="This project is a medicine box that can remind the user to take the medicine on time, it keeps track of the medicines to be taken and it indicates the dosage of medicine to be taken, all of these functions are done with the help of Arduino uno, RTC module, 16X2 lcd display and a buzzer."
+    }
+
+
+    
+       
+    
+        
+    
+
